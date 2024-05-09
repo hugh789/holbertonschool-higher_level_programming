a51e65482d348978500a65975d5f6f31d474eb1f
root <hugh.fagan123@gmail.com>
2024-05-09 16:27:22 +1000

    Style guide to original first push

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,8 +1,3 @@
 # This script prints the specified string followed by a new line
-def main():
-    print("Programming is like building a multilingual puzzle")
-    print()  # Print a new line
-
-
-if __name__ == "__main__":
-    main()
+#!/usr/bin/python3
+print("\"Programming is like building a multilingual puzzle")
