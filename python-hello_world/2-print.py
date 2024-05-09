db6b26adb01710f24c39035bf2b2fda08e27973c
root <hugh.fagan123@gmail.com>
2024-05-09 16:25:59 +1000

    Syntax fix

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,3 +1,8 @@
-#!/usr/bin/python3
-print('"Programming is like building a multilingual puzzle')
-print()
+# This script prints the specified string followed by a new line
+def main():
+    print("Programming is like building a multilingual puzzle")
+    print()  # Print a new line
+
+
+if __name__ == "__main__":
+    main()
