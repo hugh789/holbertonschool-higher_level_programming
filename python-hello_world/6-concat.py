7bf3dc2a209c223ff31cc4598860a13e28a3b452
root <hugh.fagan123@gmail.com>
2024-05-17 10:48:45 +1000

    Another attempt at changing to pycodestyle

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,7 +1,6 @@
-"""Aligned with Pycodestyle"""
+#!/usr/bin/python3
 
 str1 = "Holberton"
 str2 = "School"
-
-# Concatenate strings with f-string for clarity
-print(f"Welcome to {str1}!")
+str1 += " " + str2
+print("Welcome to {}!".format(str1))
