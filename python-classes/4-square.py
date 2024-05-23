51f055a07a059220d72a1a1c776f3cafc8e0cd17
root <hugh.fagan123@gmail.com>
2024-05-23 11:44:05 +1000

    Fixing syntax only

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -7,12 +7,12 @@ class Square:
     def __init__(self, size=0):
         """Inits Square with a size integer.
 
-            Args:
-                size: An integer size of the square.
+        Args:
+            size: An integer size of the square.
 
-            Raises:
-                TypeError: The size argument is not an integer.
-                ValueError: The size argument is negative.
+        Raises:
+            TypeError: The size argument is not an integer.
+            ValueError: The size argument is negative.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -23,23 +23,23 @@ class Square:
     def area(self):
         """Computes the area of the square."""
         return self.__size * self.__size
-    
+
     @property
     def size(self):
-        ""Gets the size of the square
+        """Gets the size of the square
 
         Returns:
-            The size attribute of the square 
-        ""
+            The size attribute of the square
+        """
         return self.__size
-    
+
     @size.setter
     def size(self, value):
-        ""Sets the size with a value
-            
-            Args:
-                value: value to assign to size 
-        ""
+        """Sets the size with a value
+
+        Args:
+            value: value to assign to size
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
