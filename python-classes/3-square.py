c4611e8e49db67425d80141bb83c6c390a92ac06
root <hugh.fagan123@gmail.com>
2024-05-23 11:26:58 +1000

    Adding a class Square that defines a square by: (based on 2-square.py)

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-"""Defines a 'Square' class with a private instance attribute"""
+"""Defines a 'Square' class with a private instance attribute."""
 
 
 class Square:
     """Definition of 'Square'"""
     def __init__(self, size=0):
-        """Inits Square with 'size' private instance attribute
+        """Inits Square with a size integer.
 
             Args:
                 size: An integer size of the square.
 
             Raises:
                 TypeError: The size argument is not an integer.
-                Value Error: The size argument is negative.
+                ValueError: The size argument is negative.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """Computes the area of the square."""
+        return self.__size * self.__size
