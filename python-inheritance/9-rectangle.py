1720bba497a7fcc04b9b8507507121f341a9b352
root <hugh.fagan123@gmail.com>
2024-05-24 11:03:15 +1000

    Adding a class Rectangle that inherits from BaseGeometry (7-base_geometry.py). (task based on 8-rectangle.py)

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -11,3 +11,11 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+
+    def area(self):
+        """Computes the area of the Rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns a description of the rectangle."""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
