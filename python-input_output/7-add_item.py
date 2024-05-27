9563fbba86f4fcf993c106ba659c13c6bf5be4c6
root <hugh.fagan123@gmail.com>
2024-05-27 12:34:14 +1000

    Fixing 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
+"""
+This module when run adds all arguments to a python list
+and save them to a file
+"""
+import sys
 
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-import json
+try:
+    obj = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    obj = []
 
+for i in range(1, len(sys.argv)):
+    obj.append(sys.argv[i])
 
-def save_to_json_file(my_obj, filename):
-    with open(filename, "w", encoding="UTF-8") as f:
-        json.dump(my_obj, f)
+save_to_json_file(obj, "add_item.json")
