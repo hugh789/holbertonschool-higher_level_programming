26a4b1987231c48b7b982a29c956a3aded20cbc9
root <hugh.fagan123@gmail.com>
2024-05-29 10:08:55 +1000

    syntax fix

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import csv
 import json
 
@@ -13,13 +12,13 @@ def convert_csv_to_json(csv_file):
  """
 
 #Reads CSV and Converts to Dictionaries with Error handling
- try:
+  try:
     with open(csv_file, 'r') as csvfile:
-        reader = csv.DictReader(csvfile)
-        data = list(reader)
-    except FileNotFoundError:
-        print(f"Error: File not found - {csv_file}")
-        return False
+      reader = csv.DictReader(csvfile)
+      data = list(reader)
+  except FileNotFoundError:
+    print(f"Error: File not found - {csv_file}")
+    return False
 
 # Serializing and Writing JSON Data
     with open('data.json', 'w') as jsonfile:
