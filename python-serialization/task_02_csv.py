813dabe1e44966b5e79b25b6b269dac25f72ca2e
root <hugh.fagan123@gmail.com>
2024-05-29 10:06:59 +1000

    Adding py file that reads  data from one format (CSV) and converting it into another format (JSON) using serialization techniques.

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import csv
+import json
+
+def convert_csv_to_json(csv_file):
+ """ Converts as CSV file to JSON format and writes it to data.sjon.
+
+    Args:
+    csv_file (str): The path to the CSV file. 
+
+    Returns: 
+        bool: True if the conversion was successful, False otherwise.
+ """
+
+#Reads CSV and Converts to Dictionaries with Error handling
+ try:
+    with open(csv_file, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        data = list(reader)
+    except FileNotFoundError:
+        print(f"Error: File not found - {csv_file}")
+        return False
+
+# Serializing and Writing JSON Data
+    with open('data.json', 'w') as jsonfile:
+       json.dump(data, jsonfile)
+    return True
+ 
