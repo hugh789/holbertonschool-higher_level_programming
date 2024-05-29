a9d67ab9e7be2b1b6aadc61d96a1f8c69c0360a4
root <hugh.fagan123@gmail.com>
2024-05-29 10:10:36 +1000

    indentation error fix

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -2,16 +2,16 @@ import csv
 import json
 
 def convert_csv_to_json(csv_file):
- """ Converts as CSV file to JSON format and writes it to data.sjon.
+  """Converts a CSV file to JSON format and writes it to data.json.
 
-    Args:
-    csv_file (str): The path to the CSV file. 
+  Args:
+      csv_file (str): The path to the CSV file.
 
-    Returns: 
-        bool: True if the conversion was successful, False otherwise.
- """
+  Returns:
+      bool: True if the conversion was successful, False otherwise.
+  """
 
-#Reads CSV and Converts to Dictionaries with Error handling
+  # Reads CSV and Converts to Dictionaries with Error handling
   try:
     with open(csv_file, 'r') as csvfile:
       reader = csv.DictReader(csvfile)
@@ -20,8 +20,7 @@ def convert_csv_to_json(csv_file):
     print(f"Error: File not found - {csv_file}")
     return False
 
-# Serializing and Writing JSON Data
-    with open('data.json', 'w') as jsonfile:
-       json.dump(data, jsonfile)
-    return True
- 
+  # Serializing and Writing JSON Data
+  with open('data.json', 'w') as jsonfile:
+    json.dump(data, jsonfile)
+  return True
