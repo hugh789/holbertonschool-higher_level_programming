73762adafc9ca64ca05605829f56b966a0aad093
root <hugh.fagan123@gmail.com>
2024-05-30 16:54:28 +1000

    Adrian suggestions

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -8,31 +8,34 @@ task_04_flask:
 #Step 1: Setting Up Flask
 from flask import Flask, jsonify, request, abort
 
+# Step 1
 app = Flask(__name__)
+
+users = {}
 # Example dictionary: username(key) whole object(value)
 # users = {"jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"}}
-users = {}
+
 
 #Step 2: Creating Your First Endpoint:
 @app.route("/")
 def home():
+    """ Prints welcome string """
     return "Welcome to the Flask API!"
-
 #Step 3: Serving JSON Data
 @app.route("/data")
 def data():
-    usernames = list(users.keys())  # List of all the usernames
+    """ Returns JSON data """
     return jsonify(list(users.keys()))
 
 #Step 4: Expanding Your API
-
 @app.route("/status")
 def status():
+    """ Prints OK """
     return "OK"
 
 @app.route("/users/<username>")
-def users_username(username):
-    # Getting whole object(value) that corresponds with username(key)
+def users_specific(username):
+    """ Get specified """
     if username not in users:
         return jsonify({"error": "User not found"}), 404
 
@@ -40,20 +43,31 @@ def users_username(username):
     output["username"] = username
 
     return jsonify(output)
-
 #Step 5: Handling Post Requests
-@app.route("/add_user", methods=['POST'])
+@app.route("/add_user", methods=["POST"])
 def add_user():
-    data = request.get_json()  # Get incoming JSON data
-    username = data.get('username')  # Parse through incoming JSON data
-    users[username] = {
-        "username": data.get("username"),
-        "name": data.get("name"),
-        "age": data.get("age"),
-        "city": data.get("city")
-        }  # Add to users dictionary
-    return jsonify({"message": "User added", "user": users[username]})  # Confirmation message
+    """ adds a new user to the dict """
+    if request.get_json() is None:
+        abort(400, "Not a JSON")
 
+    req_data = request.get_json()
+
+    if "username" not in req_data:
+        return jsonify({"error": "Username is required"}), 400
+
+    users[req_data["username"]] = {
+        "name": req_data["name"],
+        "age": req_data["age"],
+        "city": req_data["city"]
+    }
+
+    output = {
+        "username": req_data["username"],
+        "name": req_data["name"],
+        "age": req_data["age"],
+        "city": req_data["city"]
+    }
+    return jsonify({"message": "User added", "user": output}), 201
 #Step 2: Run Flask developer server + enable debug (by deafult)
 # Set debug=True for the server to auto-reload when there are changes
 if __name__ == "__main__":
