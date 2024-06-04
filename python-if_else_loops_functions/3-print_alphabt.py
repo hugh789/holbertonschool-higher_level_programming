530553f967eccf9cb86b40de74f0f8fd143e9dbe
root <hugh.fagan123@gmail.com>
2024-06-04 16:54:15 +1000

    pycode style

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for x in range(ord('a'), ord('z') + 1):
-    if x != ord('q') and x != ord('e'):
-        print("{:c}".format(x), end='')
+
+for char in range(ord('a'), ord('z') + 1):
+    if char not in ('q', 'e'):
+        print(chr(char), end='')
