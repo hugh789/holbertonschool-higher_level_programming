07101b06b028107fcadeefc2021b3785a1d21708
root <hugh.fagan123@gmail.com>
2024-06-04 19:37:45 +1000

    style

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
-def print_list_integer(my_list=[]):
-    for i in range(0, len(my_list)):
-        print("{:d}".format(my_list[i]))
-        
+
+def print_list_integer(my_list=()):
+    """Prints the integers in a list, one per line.
+
+    Args:
+        my_list (list, optional): The list of integers to print. Defaults to an empty list.
+    """
+
+    for i in range(len(my_list)):
+        print(my_list[i])  # Use direct integer printing
