1f902c1a936ff181348215dcb98356c8e9bde8b8
root <hugh.fagan123@gmail.com>
2024-06-04 17:14:13 +1000

    pycodestyle fixx

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 
+# multiples of 3 = Fizz
+# multiples of 5 = Buzz
+# multiples of 3 and 5 = FizzBuzz
 
 def fizzbuzz():
-    for num in range(1, 101):
-        if num % 15 == 0:
-            print("FizzBuzz", end='')  # Remove extra space
-        elif num % 3 == 0:
-            print("Fizz", end='')  # Remove extra space
-        elif num % 5 == 0:
-            print("Buzz", end='')  # Remove extra space
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif number % 3 == 0:
+            print("Fizz ", end="")
+        elif number % 5 == 0:
+            print("Buzz ", end="")
         else:
-            print(num, end=' ')  # Use f-string or direct formatting
-
+            print("{} ".format(number), end="")
