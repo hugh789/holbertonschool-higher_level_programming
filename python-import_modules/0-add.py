7a5d06455a05f411b1bdc318a276b30d7a55bacc
root <hugh.fagan123@gmail.com>
2024-06-04 20:12:05 +1000

    d

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python3
-if __name__ == '__main__':
+#!/usr/bin/python3
+if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+    added = add(a, b)
+    print("{} + {} = {}".format(a, b, added))
