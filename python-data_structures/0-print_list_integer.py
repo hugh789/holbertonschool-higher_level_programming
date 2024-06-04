9783fe7bf364c182b55f8f7334f79a2e073a183c
root <hugh.fagan123@gmail.com>
2024-06-04 19:41:39 +1000

    style

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    for i in range(0, len(my_list)):
+    for i in range(len(my_list)):
+        my_list[i]
         print("{:d}".format(my_list[i]))
-        
