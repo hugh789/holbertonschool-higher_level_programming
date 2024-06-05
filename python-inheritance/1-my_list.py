f51cd75e346231a65c26d83973fe781ae3ea1177
root <hugh.fagan123@gmail.com>
2024-06-05 11:49:05 +1000

    style fix

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 
-"""Defines a MyList subclass."""
-
-
 class MyList(list):
     """A subclass of list with a print_sorted method."""
 
     def print_sorted(self):
         """Prints list sorted in ascending order."""
         print(sorted(self))
-        
