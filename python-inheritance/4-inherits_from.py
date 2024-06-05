ecf8bc3232317a3b37f99c089d53bce1b5df8623
root <hugh.fagan123@gmail.com>
2024-06-05 11:55:08 +1000

    style

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-
-"""Defines a inherits_from() function."""
+"""
+4-inhertis_from:
+    This module contains the inherits_from method
+"""
 
 
 def inherits_from(obj, a_class):
-    """Checks if an object inherits from a class.
-
-    Args:
-        obj: Object to check
-        a_class: Class to check with
-
-    Returns:
-        True if the object inherits from the class or False
     """
-    return isinstance(obj, a_class) and type(obj) != a_class
+    Returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class ; otherwise False.
+    """
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
+        return True
+    else:
+        return False
