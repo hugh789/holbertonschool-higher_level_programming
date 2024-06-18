f49b70053ec13a5f1742cc51a1559bec148b547f
root <hugh.fagan123@gmail.com>
2024-06-18 17:38:03 +1000

    User and permission tasks

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
--- list all privileges for user_0d_1 and user_0d_2 on localhost server
-
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Lists all privileges of MySQL users user_0d_1 and user_0d_2
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
