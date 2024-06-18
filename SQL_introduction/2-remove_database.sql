cfc56f21d9890093e54ee33db0b1cbf4f659c170
root <hugh.fagan123@gmail.com>
2024-06-18 12:00:05 +1000

    updated 2-remove database

---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- deletes the database hbtn_0c_0 in your MySQL server
+-- if it doesn't exist, it shouldn't fail
+-- not allowed to use SELECT or SHOW statements
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
