505380f2479e95eb94e02e80276af9165957c4b8
root <hugh.fagan123@gmail.com>
2024-06-21 11:39:57 +1000

    Abdu help task 7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""Nameless module to suck data out from the database
-"""
+"""a script that lists all cities from the database hbtn_0e_4_usa"""
 import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1],
-        sys.argv[2],
-        sys.argv[3]),
-        pool_pre_ping=True
-    )
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    rows = session.query(State).all()
-    for row in rows:
-        print("{0}: {1}".format(row.id, row.name))
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """
-First class definition State.
-"""
+Module for State class and Base instance.
 
+Defines:
+    - Base: SQLAlchemy declarative base
+    - State: ORM class for 'states' table
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -11,8 +14,17 @@ Base = declarative_base()
 
 class State(Base):
     """
-    Class State that inherits from Base
+    Represents a state with id and name attributes.
+
+    Attributes:
+        __tablename__ (str): The table name
+        id (Column): The state's id, primary key
+        name (Column): The state's name
     """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer,
+                unique=True,
+                autoincrement=True,
+                nullable=False,
+                primary_key=True)
     name = Column(String(128), nullable=False)
