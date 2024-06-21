f8b3bc259ea52744899b6d7d451496465354776d
root <hugh.fagan123@gmail.com>
2024-06-21 14:18:04 +1000

    state

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""a script that lists all cities from the database hbtn_0e_4_usa"""
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import State
+"""Nameless module to declare tables for db hbtn_0e_6_usa
+"""
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
-    got_it = 0
-    for state in session.query(State):
-        if sys.argv[4] == state.name:
-            print("{}".format(state.id))
-            got_it = 1
-    if got_it == 0:
-        print("Not found")
+Base = declarative_base()
+
+
+class State(Base):
+    """Class to declare the states database table
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
