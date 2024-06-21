dfb71ff42b732971c1204639f203835c655545b8
root <hugh.fagan123@gmail.com>
2024-06-21 14:09:38 +1000

    change

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,30 +1,18 @@
-#!/usr/bin/python3
-"""
-Module for State class and Base instance.
 
-Defines:
-    - Base: SQLAlchemy declarative base
-    - State: ORM class for 'states' table
+#!/usr/bin/python3
+"""Nameless module to declare tables for db hbtn_0e_6_usa
 """
-from sqlalchemy import Column, Integer, String
+
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
 
 class State(Base):
-    """
-    Represents a state with id and name attributes.
-
-    Attributes:
-        __tablename__ (str): The table name
-        id (Column): The state's id, primary key
-        name (Column): The state's name
+    """Class to declare the states database table
     """
     __tablename__ = 'states'
-    id = Column(Integer,
-                unique=True,
-                autoincrement=True,
-                nullable=False,
-                primary_key=True)
+
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
