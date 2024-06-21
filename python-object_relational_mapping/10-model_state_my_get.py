a8e51312574d1e5a3e7d23fe3398afc10be9b40a
root <hugh.fagan123@gmail.com>
2024-06-21 14:16:41 +1000

    Abdu help

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
-
-"""Nameless module to suck data out from the database
-"""
+"""a script that lists all cities from the database hbtn_0e_4_usa"""
 import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1],
-        sys.argv[2],
-        sys.argv[3]),
-        pool_pre_ping=True
-    )
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state_name = sys.argv[4]
-
-    query = session.query(State).where(State.name == state_name)
-
-    if query.count() == 0:
+    got_it = 0
+    for state in session.query(State):
+        if sys.argv[4] == state.name:
+            print("{}".format(state.id))
+            got_it = 1
+    if got_it == 0:
         print("Not found")
-    else:
-        row = query.limit(1).one()
-        print("{0}".format(row.id))
